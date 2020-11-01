78161ea3306b624e42df7c695ad99cdd222bdbb5
edd <zeedymarina@gmail.com>
2020-11-01 17:32:07 -0500

    homeWork_9

---
+++ MyJava/src/homeWork/restaurant_Methods.java
@@ -0,0 +1,40 @@
+package homeWork;
+
+public class restaurant_Methods {
+
+    public String restName = "";
+    public int guestCapacity = 0;
+    public int guestCount = 1;
+
+    public restaurant_Methods(String name, int guests){
+        this.restName = name;
+        this.guestCapacity = guests;
+    }
+
+    public void info(){
+        System.out.println("Guest capacity remain "+guestCapacity);
+        System.out.println("People in the restaurant "+guestCount);
+    }
+    //Create a method that tells user if the seats are available
+    public int guestCap(int gIn){
+        int data = guestCapacity - gIn;
+        guestCapacity=data;
+        return data;
+    }
+    public int geCount(int gCou){
+        int data = guestCount+gCou;
+        guestCount=data;
+        return data;
+    }
+    public void gLeave(int leave){
+        this.guestCapacity = guestCapacity+leave;
+        this.guestCount = guestCount - leave;
+    }
+    public void summery(){
+        System.out.println("Restaurant name "+restName);
+        System.out.println("Guest present "+guestCount);
+        System.out.println("Capacity "+guestCapacity);
+    }
+
+
+}
---
+++ MyJava/src/practice/intArray_find_reoeatNumber.java
@@ -0,0 +1,32 @@
+package practice;
+
+public class test {
+    public static class MaxRepeatingBruteForce {
+        public void MaxRepeatingElement(int [] arrA){
+            int maxCounter = 0;
+            int element=0;
+            for (int i = 0; i <arrA.length ; i++) {
+                int counter =1;
+                for (int j = i+1; j <arrA.length ; j++) {
+                    if(arrA[i]==arrA[j]){
+                        counter++;
+                    }
+                }
+                if(maxCounter<counter){
+                    maxCounter=counter;
+                    element = arrA[i];
+                }
+            }
+            System.out.println("Element repeating maximum no of times: " + element + ", maximum count: " + maxCounter);
+        }
+
+        public static void main(String[] args) {
+            int [] arrA = {4, 1, 5, 5, 1, 5, 9, 8, 6, 5, 3, 2, 4, 7};
+            MaxRepeatingBruteForce m = new MaxRepeatingBruteForce();
+            m.MaxRepeatingElement(arrA);
+
+            String[] name = {"happy", "peace", "joy", "grow", "peace", "happy", "peace", "grow"};
+        }
+    }
+
+}
