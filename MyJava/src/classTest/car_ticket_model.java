68deb45c6fc268b0b698efcb08f82141a6453a85
edd <zeedymarina@gmail.com>
2020-11-07 21:35:13 -0500

    HW_11

---
+++ MyJava/src/classTest/car_ticket_model.java
@@ -0,0 +1,31 @@
+package classTest;
+
+public class hhhh {
+    public static void main(String[] args) {
+        int userspeed = 88;
+        speed(userspeed);
+        points(userspeed);
+    }
+        public static int speedLimit = 70;
+        public static int speed ;
+        public static int point = 12;
+
+        public static void speed(int user){
+            speed = user;
+            System.out.println("Speed limit 70, your speed is "+speed);
+        }
+
+        public static void points(int numb){
+            int res = 0;
+            if ( speed > speedLimit)
+            {
+                res = (speed-speedLimit)/5;
+                point= point-res;
+            }if(point<0){
+                System.out.println("You have no more points in your licence.");
+            }
+            System.out.println("you have "+point+" points.");
+        }
+
+
+}
