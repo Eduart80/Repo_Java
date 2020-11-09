bf9679d7627ffd1ea0942ddd6e1c31080ca641c6
edd <zeedymarina@gmail.com>
2020-11-09 14:55:34 -0500

    hW_11

--- MyJava/src/homeWork/homeWork_11.java
+++ MyJava/src/homeWork/homeWork_11.java
@@ -2,8 +2,7 @@ package homeWork;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class homeWork_11 {
     public static void main(String[] args) throws ParseException {
@@ -24,35 +23,36 @@ public class homeWork_11 {
 
         }
 
+        String[] arr = {"green","red", "red","blue",
+                "yellow", "grey", "green",
+                "red", "grey","green", "yellow", "yellow", "grey", "blue" , "yellow", "grey",
+                "green", "blue", "hot", "yellow",  "green", "blue", "green", "green","White","White"};
 
-
-
-
+        /// find
+        System.out.println( findIn(arr));
     }
+
+    public static Map<String, Integer> findIn(String[] name){
+        Map<String, Integer> wordCount = new HashMap<>();
+        //List<String> list = new ArrayList<>();
+        for (int i=0; i<name.length; i++){
+            String wordsA = name[i];
+            int count = 0;
+            for(int j=0;j<name.length;j++) {
+                if (j<i && name[i]==name[j]) {
+                    break;
+                }
+                if(name[i]==name[j]){
+                    count++;
+                }
+            }
+            if (count > 1 && !wordCount.containsKey(wordsA.toLowerCase())) {
+                wordCount.put(wordsA.toLowerCase() ,count);
+            }
+        }
+
+        return wordCount;
+    }
+
 }
-    /**String[] arr = {"green", "blue", "red", "yellow", "grey", "green", "red", "grey",
-            "green",  "red", "yellow", "yellow", "grey", "blue" , "yellow", "grey",
-            "green", "blue",  "yellow", "grey", "green", "blue", "green", "green",
-            "green", "green"};
-*/
-/**
- * Question 1:
- * Create a method that will take String-array as input
- *
- * If there is a color with maximum count, return the color name
- * If there are two or more colors with same number, return all.
- */
 
-
-/**
- * Question 2:
- * Create a method that will take an int as input.
- * return the timeline (hour am/pm) with interval of 2 hour from the current hour.
- */
-/**
- System.out.println(generateTimline(5));
- 10pm 12am 2am 4am 6am
-
- System.out.println(generateTimline(8)); // executing method at 9am
- 11am 1pm 3pm 5pm 7pm 9pm 11pm 1am
- */
